49414026161ec49051285d53b9e5f69a2d160662
hendisantika <hendisantika@yahoo.co.id>
2024-03-19 08:20:09 +0700

    Add commandLineRunner 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootvideocallapp/SpringBootVideoCallAppApplication.java
+++ src/main/java/id/my/hendisantika/springbootvideocallapp/SpringBootVideoCallAppApplication.java
@@ -1,7 +1,11 @@
 package id.my.hendisantika.springbootvideocallapp;
 
+import id.my.hendisantika.springbootvideocallapp.user.User;
+import id.my.hendisantika.springbootvideocallapp.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootVideoCallAppApplication {
@@ -10,4 +14,28 @@ public class SpringBootVideoCallAppApplication {
         SpringApplication.run(SpringBootVideoCallAppApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            UserService service
+    ) {
+        return args -> {
+            service.register(User.builder()
+                    .username("yuji")
+                    .email("yuji@yopmail.com")
+                    .password("Naruto2024!")
+                    .build());
+
+            service.register(User.builder()
+                    .username("megumi")
+                    .email("jomegumi@yopmail.com")
+                    .password("Naruto2024!")
+                    .build());
+
+            service.register(User.builder()
+                    .username("gojo")
+                    .email("gojo@yopmail.com")
+                    .password("Naruto2024!")
+                    .build());
+        };
+    }
 }
