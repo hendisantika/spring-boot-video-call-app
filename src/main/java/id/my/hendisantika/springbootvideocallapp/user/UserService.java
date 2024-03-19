2f7925aa91d53caef0db63d0bdd0624c41031cf1
hendisantika <hendisantika@yahoo.co.id>
2024-03-19 08:18:19 +0700

    Add UserService 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootvideocallapp/user/UserService.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.springbootvideocallapp.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-video-call-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/19/24
+ * Time: 08:17
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class UserService {
+    private static final List<User> USERS_LIST = new ArrayList<>();
+}
