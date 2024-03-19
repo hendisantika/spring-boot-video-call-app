ea0cef05a212e6714d4703d72f295672fd25ce47
hendisantika <hendisantika@yahoo.co.id>
2024-03-19 08:18:41 +0700

    Add login in UserService 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootvideocallapp/user/UserService.java
+++ src/main/java/id/my/hendisantika/springbootvideocallapp/user/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,5 +23,18 @@ public class UserService {
     public void register(User user) {
         user.setStatus("online");
         USERS_LIST.add(user);
+    }
+
+    public User login(User user) {
+        var userIndex = IntStream.range(0, USERS_LIST.size())
+                .filter(i -> USERS_LIST.get(i).getEmail().equals(user.getEmail()))
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        var cUser = USERS_LIST.get(userIndex);
+        if (!cUser.getPassword().equals(user.getPassword())) {
+            throw new RuntimeException("Password incorrect");
+        }
+        cUser.setStatus("online");
+        return cUser;
     }
 }
