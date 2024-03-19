3c0a4985096db50bbac6d21631fb60ffe1f81b51
hendisantika <hendisantika@yahoo.co.id>
2024-03-19 08:21:32 +0700

    Add handle in UserController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootvideocallapp/user/UserController.java
+++ src/main/java/id/my/hendisantika/springbootvideocallapp/user/UserController.java
@@ -2,7 +2,9 @@ package id.my.hendisantika.springbootvideocallapp.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 /**
  * Created by IntelliJ IDEA.
@@ -48,5 +52,13 @@ public class UserController {
     @GetMapping
     public List<User> findAll() {
         return service.findAll();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handle(Exception ex) {
+        ex.printStackTrace();
+        return ResponseEntity
+                .status(INTERNAL_SERVER_ERROR)
+                .body(ex.getMessage());
     }
 }
